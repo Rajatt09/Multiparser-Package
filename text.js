3d64ef2e63cd05ed1933bfb7abf4406eb66dd6bc
Rajatt09 <rajatbhati9559@gmail.com>
2024-12-27 02:49:59 +0530

    updated readme.md

--- text.js
+++ text.js
@@ -1,22 +1,21 @@
-import DocumentTextExtractor from './index.js';
+import DocumentTextExtractor from "./index.js";
 
 async function main() {
   const extractor = new DocumentTextExtractor();
-  
+
   try {
-    const pdf_text = await extractor.extract('text_pdf.pdf');
-    console.log('Extracted Text:', pdf_text);
+    const pdf_text = await extractor.extract("text_pdf.pdf");
+    console.log("Extracted Text:", pdf_text);
 
     // Extract text from a DOCX file
-    const docx_text = await extractor.extract('test_docx.docx');
-    console.log('Extracted Text:', docx_text);
+    const docx_text = await extractor.extract("test_docx.docx");
+    console.log("Extracted Text:", docx_text);
 
     // Extract text from a PPTX file
-    const pptx_text = await extractor.extract('test_ppt.pptx');
-    console.log('Extracted Text:', pptx_text);
-
+    const pptx_text = await extractor.extract("test_ppt.pptx");
+    console.log("Extracted Text:", pptx_text);
   } catch (error) {
-    console.error('Error:', error.message);
+    console.error("Error:", error.message);
   }
 }
 
